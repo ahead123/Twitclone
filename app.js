6f0269aadd73e1199ea4fc2a07eccbd6ee630054
Arthur Head <arthurallenhead@yahoo.com>
2014-11-16 00:25:04 -0800

    added bootstrap stylingcontainers and rows

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ var main = function(){
 
 
 	// inserts an li with the attached status update on button click
-    $('.btn').click(function(){
+    $('#poster').click(function(){
         var post = $('.status-box').val();
         $('<li class="text-info">').html(post).prependTo('.posts');
+        // $('<button id="bye" class="btn btn-danger">').html('Delete').prependTo('.posts');
         $('.status-box').val('');
-         $('.counter').text("140");
+        $('.counter').text("140");
+        $('.#poster').addClass('disabled');
     });
-
 
     // updates the character count while typing in the status box
     $('.status-box').keyup(function() {
@@ -16,15 +17,15 @@ var main = function(){
    		var charactersLeft = 140 - postLength;
     	$('.counter').text(charactersLeft);
   		if(charactersLeft < 0){
-       	 	$('.btn').addClass('disabled');
+       	 	$('#poster').addClass('disabled');
     	} else if(charactersLeft === 140){
-        	$('.btn').addClass('disabled');
+        	$('#poster').addClass('disabled');
     	} else{
-        	$('.btn').removeClass('disabled');
+        	$('#poster').removeClass('disabled');
     	};
   	});
   
-  	$('.btn').addClass('disabled');
+  	$('#poster').addClass('disabled');
 };
 
 $(document).ready(main);
