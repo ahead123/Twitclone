a1eedcfa941c416b361fab3e9d5e7e4e4161bfd4
Arthur Head <arthurallenhead@yahoo.com>
2014-11-15 23:32:02 -0800

    add character count

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 var main = function(){
+
+
+	// inserts an li with the attached status update on button click
     $('.btn').click(function(){
         var post = $('.status-box').val();
-        $('<li>').html(post).prependTo('.posts');
+        $('<li class="text-info">').html(post).prependTo('.posts');
+        $('.status-box').val('');
     });
+
+
+    // updates the character count while typing in the status box
+    $('.status-box').keyup(function() {
+    	var postLength = $(this).val().length;
+   		var charactersLeft = 140 - postLength;
+    	$('.counter').text(charactersLeft);
+  	});
 };
 
 $(document).ready(main);
